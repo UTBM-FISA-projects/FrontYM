3de75a347fc4c519fbd41d813f9e7c0b26e56ddc
Valentin <valentin.d77@free.fr>
2021-10-09 21:20:21 +0200

    reformat de Login.jsx + ajout favicon

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 import { Button, Card, Container, FloatingLabel, Form, Stack } from 'react-bootstrap';
 
 const Login = () => (
-        <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-            <Card>
-                <Card.Body>
-                    <Form>
-                        <FloatingLabel label="Email" className="mb-3">
-                            <Form.Control required placeholder="test@email.com" type="email" size="lg" />
-                        </FloatingLabel>
-                        <FloatingLabel label="Mot de passe" className="mb-3">
-                            <Form.Control required placeholder="Mot de passe" size="lg" type="password" />
-                        </FloatingLabel>
-                        <Stack gap={2} className="mt-4">
-                            <Button variant="success" type="submit">Connexion</Button>
-                            <Button size="sm" variant="outline-secondary">S'inscrire</Button>
-                        </Stack>
-                    </Form>
-                </Card.Body>
-            </Card>
-        </Container>
-    );
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+        <Card>
+            <Card.Body>
+                <Form>
+                    <FloatingLabel label="Email" className="mb-3">
+                        <Form.Control required placeholder="test@email.com" type="email" size="lg" />
+                    </FloatingLabel>
+                    <FloatingLabel label="Mot de passe" className="mb-3">
+                        <Form.Control required placeholder="Mot de passe" size="lg" type="password" />
+                    </FloatingLabel>
+                    <Stack gap={2} className="mt-4">
+                        <Button variant="success" type="submit">Connexion</Button>
+                        <Button size="sm" variant="outline-secondary">S'inscrire</Button>
+                    </Stack>
+                </Form>
+            </Card.Body>
+        </Card>
+    </Container>
+);
 
 export default Login;

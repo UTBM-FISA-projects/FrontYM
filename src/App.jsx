9880853081560797cf77c7ac2c1312ae14c16262
Kilian <37240228+Beladric@users.noreply.github.com>
2021-10-14 22:48:34 +0200

    Page Chantiers (#2)
    Ajout de la page chantier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import { Login, Inscription } from './pages';
+import { DashboardChantier, Inscription, Login } from './pages';
 
 const App = () => (
     <BrowserRouter>
         <Switch>
             <Route component={Login} path="/login" exact />
             <Route component={Inscription} path="/Inscription" exact />
+            <Route component={DashboardChantier} path="/DashboardChantier" exact />
             <Redirect to="/login" />
         </Switch>
     </BrowserRouter>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Login } from './Login';
 export { Inscription } from './Inscription';
+export { DashboardChantier } from './DashboardChantier';

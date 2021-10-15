1c768286f5e48435dc2c9479b8c09b0cc9b84902
Valentin <valentin.d77@free.fr>
2021-10-15 19:34:05 +0200

    utilisation de dossier pour les pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
-export { default as Login } from './Login';
-export { default as Inscription } from './Inscription';
+export { Login } from './Login';
+export { Inscription } from './Inscription';
 export { Availability } from './Availability';

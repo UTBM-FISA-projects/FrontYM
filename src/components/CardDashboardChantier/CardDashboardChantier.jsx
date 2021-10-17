7338f3f99133b6eed35bd70a1e7d5385ec763e9e
Kilian <kilian.goetz@yahoo.fr>
2021-10-17 21:27:33 +0200

    CardDashboardChantier
    Ajout de la card DashboardChantier version prestataire

--- src/components/CardDashboardChantier/CardDashboardChantier.jsx
+++ src/components/CardDashboardChantier/CardDashboardChantier.jsx
@@ -1,50 +1,61 @@
 import React from 'react';
-import { Badge, Card, Container, ProgressBar } from 'react-bootstrap';
+import { Badge, Card, ProgressBar } from 'react-bootstrap';
 import { ThreeDotsVertical } from 'react-bootstrap-icons';
 import { theme } from '../../utils';
+import PropTypes from 'prop-types';
 
 const progress = 60;
 
-const CardDashboardChantier = () => (
-    <Container className="text-center">
-        <Card>
-            <Card.Body>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm text-start">
-                            <h4>Chantier 1 Raedersheim</h4>
+const CardDashboardChantier = ({ userTypes }) => (
+    <Card>
+        <Card.Body>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm text-start">
+                        <h5>{userTypes !== 'prestataire' ? 'Chantier 1 Raedersheim' : 'Prestation sur chantier 8 Illkirch-Graffenstanden'}</h5>
+                        {userTypes !== 'prestataire' && (
                             <div className="mt-4">
                                 <h5>Progression générale</h5>
                                 <p>Progression du chantier</p>
                                 <ProgressBar now={progress} label={`${progress}%`} />
                                 <Badge bg="light" text="dark" className="mt-3">Temps de travail 0h/800h</Badge>
                             </div>
+                        )}
+                        {userTypes === 'prestataire' && (
                             <div className="mt-4">
-                                <h5>Responsable</h5>
-                                <p>Jean-Phillipe BTP</p>
-                                <Badge bg="light" text="dark">Conducteur de travaux Jean-Phillipe</Badge>
+                                <h5>Prochaine intervention</h5>
+                                <Badge bg="light" text="dark" className="mt-3">Mercredi 27 octobre 2021 à 13h</Badge>
                             </div>
-                        </div>
-                        <div className="col-1">
-                            <hr style={{ color: 'secondaryDark', width: 1, height: 250 }} />
-                        </div>
-                        <div className="col-sm text-start mt-3">
-                            <h5>Mission en cours</h5>
-                            <Card style={{ boxShadow: `0 0 2px ${theme.primaryLight}` }}>
-                                <Card.Body>
-                                    <Card.Title>Électricité</Card.Title>
-                                    <Card.Text>Mettre aux normes l'entièreté des bâtiments</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div className="col-md-auto">
-                            <ThreeDotsVertical className="position-fixed text-top-right" />
+                        )}
+                        <div className="mt-4">
+                            <h5>Responsable</h5>
+                            <p>Jean-Phillipe BTP</p>
+                            <Badge bg="light" text="dark">Conducteur de travaux Jean-Phillipe</Badge>
                         </div>
                     </div>
+                    <div className="col-1">
+                        <hr style={{ color: 'secondaryDark', width: 1, height: 250 }} />
+                    </div>
+                    <div className="col-sm text-start mt-3">
+                        <h5>{userTypes !== 'prestataire' ? 'Mission en cours' : 'Missions assignées'}</h5>
+                        <Card style={{ boxShadow: `0 0 2px ${theme.primaryLight}` }}>
+                            <Card.Body>
+                                <Card.Title>Électricité</Card.Title>
+                                <Card.Text>Mettre aux normes l'entièreté des bâtiments</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-md-auto">
+                        <ThreeDotsVertical className="position-absolute text-top-right" />
+                    </div>
                 </div>
-            </Card.Body>
-        </Card>
-    </Container>
+            </div>
+        </Card.Body>
+    </Card>
 );
+
+CardDashboardChantier.propTypes = {
+    userTypes: PropTypes.string,
+};
 
 export default CardDashboardChantier;

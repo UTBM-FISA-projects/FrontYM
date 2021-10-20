950cde1e3d5bf1b351d9164ca496175e02ecc9b3
Valentin <valentin.d77@free.fr>
2021-10-20 19:35:17 +0200

    Bouton custom avec loading

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as CardDashboardChantier } from './CardDashboardChantier/CardDashboardChantier';
+export { default as Button } from './Button/Button';

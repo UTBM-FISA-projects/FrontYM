80e9b30c28b3e60eda35e5a4c9f06b6cb5815dba
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2021-10-20 08:18:41 +0200

    affichage du nom d'utilisateur sur la navbar (#7)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,29 +6,39 @@ import { PersonSquare } from 'react-bootstrap-icons';
 
 import NotificationPopover from './NotificationPopover';
 
-import { theme } from '../../utils';
+import { request, theme } from '../../utils';
 
 import logo from '../../assets/logo_light.png';
 
-const Navbar = () => (
-    <BsNavbar sticky="top" className="mb-3" bg="dark" variant="dark" expand="md">
-        <BsNavbar.Brand as={Link} to="/chantiers">
-            <img src={logo} height={30} alt="Logo Yard Management" />
-        </BsNavbar.Brand>
-        <BsNavbar.Toggle />
-        <BsNavbar.Collapse>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to="/profil">Profil</Nav.Link>
-                <Nav.Link as={Link} to="/employes">Employés</Nav.Link>
-                <Nav.Link as={Link} to="/disponibilites">Disponibilités</Nav.Link>
-            </Nav>
-            <Nav>
-                <NotificationPopover />
-                <Nav.Link as={Link} to="/profil">Utilisateur</Nav.Link>
-                <Nav.Link as="div"><PersonSquare color={theme.secondaryDark} size={30} /></Nav.Link>
-            </Nav>
-        </BsNavbar.Collapse>
-    </BsNavbar>
-);
+const Navbar = () => {
+    const [user, setUser] = React.useState({});
+
+    React.useEffect(() => {
+        request.get('/api/users/current').then(r => {
+            setUser(r);
+        });
+    }, []);
+
+    return (
+        <BsNavbar sticky="top" className="mb-3" bg="dark" variant="dark" expand="md">
+            <BsNavbar.Brand as={Link} to="/chantiers">
+                <img src={logo} height={30} alt="Logo Yard Management" />
+            </BsNavbar.Brand>
+            <BsNavbar.Toggle />
+            <BsNavbar.Collapse>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/profil/">Profil</Nav.Link>
+                    <Nav.Link as={Link} to="/employes">Employés</Nav.Link>
+                    <Nav.Link as={Link} to="/disponibilites">Disponibilités</Nav.Link>
+                </Nav>
+                <Nav>
+                    <NotificationPopover />
+                    <Nav.Link as={Link} to="/profil">{user.name}</Nav.Link>
+                    <Nav.Link as="div"><PersonSquare color={theme.secondaryDark} size={30} /></Nav.Link>
+                </Nav>
+            </BsNavbar.Collapse>
+        </BsNavbar>
+    );
+};
 
 export default Navbar;

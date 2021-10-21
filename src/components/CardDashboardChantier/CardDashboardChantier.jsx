b1afdbc414dc0d823c043ed8180d93f81169a88f
Valentin <valentin.d77@free.fr>
2021-10-21 21:54:01 +0200

    affichage entreprise responsable

--- src/components/CardDashboardChantier/CardDashboardChantier.jsx
+++ src/components/CardDashboardChantier/CardDashboardChantier.jsx
@@ -7,19 +7,30 @@ import { EyeFill, ThreeDotsVertical } from 'react-bootstrap-icons';
 
 import { TaskList } from '../TaskList';
 
-import { theme, yardShape } from '../../utils';
+import { isValidId, request, theme, yardShape } from '../../utils';
 import { EstimatedTime } from '../EstimatedTime';
 
 const CardDashboardChantier = ({ userTypes, yard }) => {
     const {
         id_yard,
         name,
-        supervisor,
+        supervisor: {
+            name: supervisorName,
+            id_enterprise,
+        },
         done_tasks,
         total_tasks,
         total_estimated_time,
         total_time_spent,
     } = yard;
+
+    const [enterprise, setEnterprise] = React.useState({});
+
+    React.useEffect(() => {
+        if (isValidId(id_enterprise)) {
+            request.get(`/api/users/${id_enterprise}`).then(setEnterprise);
+        }
+    }, [id_enterprise]);
 
     const progress = Math.round(done_tasks * 100 / total_tasks);
 
@@ -36,22 +47,22 @@ const CardDashboardChantier = ({ userTypes, yard }) => {
                         </Link>
                         {userTypes !== 'prestataire' && (
                             <div className="mt-4">
-                                <h5>Progression générale</h5>
-                                <p>Progression du chantier</p>
+                                <h4>Progression générale</h4>
+                                <div>Progression du chantier</div>
                                 <ProgressBar className="mb-2" now={progress} label={`${progress}%`} />
                                 <EstimatedTime spentTime={total_time_spent} estimatedTime={total_estimated_time} />
                             </div>
                         )}
                         {userTypes === 'prestataire' && (
                             <div className="mt-4">
-                                <h5>Prochaine intervention</h5>
+                                <h4>Prochaine intervention</h4>
                                 <Badge bg="light" text="dark" className="mt-3">Mercredi 27 octobre 2021 à 13h</Badge>
                             </div>
                         )}
                         <div className="mt-4">
-                            <h5>Responsable</h5>
-                            <p>Jean-Phillipe BTP</p>
-                            <Badge bg="light" text="dark">Conducteur de travaux {supervisor.name}</Badge>
+                            <h4>Responsable</h4>
+                            <div>{enterprise.name}</div>
+                            <Badge bg="light" text="dark">Conducteur de travaux {supervisorName}</Badge>
                         </div>
                     </Col>
                     <Col>

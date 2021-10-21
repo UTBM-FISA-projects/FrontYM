74d0e7041a665e5595d59393a460f42c9cf5a0ef
Valentin <valentin.d77@free.fr>
2021-10-21 16:32:58 +0200

    link api chantiers

--- src/components/CardDashboardChantier/CardDashboardChantier.jsx
+++ src/components/CardDashboardChantier/CardDashboardChantier.jsx
@@ -1,58 +1,68 @@
 import React from 'react';
-import { Badge, Card, Col, ProgressBar, Row } from 'react-bootstrap';
-import { ThreeDotsVertical } from 'react-bootstrap-icons';
-import { theme } from '../../utils';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { Badge, Card, Col, ProgressBar, Row } from 'react-bootstrap';
+import { EyeFill, ThreeDotsVertical } from 'react-bootstrap-icons';
+
+import { TaskList } from '../TaskList';
+
+import { theme, yardShape } from '../../utils';
 
 const progress = 60;
 
-const CardDashboardChantier = ({ userTypes }) => (
-    <Card>
-        <Card.Body>
-            <Row className="text-start">
-                <Col style={{ borderRight: '1px #c2c2c2 solid' }}>
-                    <h5>{userTypes !== 'prestataire' ? 'Chantier 1 Raedersheim' : 'Prestation sur chantier 8 Illkirch-Graffenstanden'}</h5>
-                    {userTypes !== 'prestataire' && (
+const CardDashboardChantier = ({ userTypes, yard }) => {
+    const {
+        id_yard,
+        name,
+        supervisor,
+    } = yard;
+
+    return (
+        <Card>
+            <Card.Body>
+                <Row className="text-start">
+                    <Col style={{ borderRight: '1px #c2c2c2 solid' }}>
+                        <Link to={`/chantiers/${id_yard}`} style={{ color: theme.primaryDark }}>
+                            <h3>
+                                {userTypes === 'prestataire' && 'Prestation sur '}{name}
+                                {' '}<EyeFill size="0.8em" />
+                            </h3>
+                        </Link>
+                        {userTypes !== 'prestataire' && (
+                            <div className="mt-4">
+                                <h5>Progression générale</h5>
+                                <p>Progression du chantier</p>
+                                <ProgressBar now={progress} label={`${progress}%`} />
+                                <Badge bg="light" text="dark" className="mt-3">Temps de travail 0h/800h</Badge>
+                            </div>
+                        )}
+                        {userTypes === 'prestataire' && (
+                            <div className="mt-4">
+                                <h5>Prochaine intervention</h5>
+                                <Badge bg="light" text="dark" className="mt-3">Mercredi 27 octobre 2021 à 13h</Badge>
+                            </div>
+                        )}
                         <div className="mt-4">
-                            <h5>Progression générale</h5>
-                            <p>Progression du chantier</p>
-                            <ProgressBar now={progress} label={`${progress}%`} />
-                            <Badge bg="light" text="dark" className="mt-3">Temps de travail 0h/800h</Badge>
+                            <h5>Responsable</h5>
+                            <p>Jean-Phillipe BTP</p>
+                            <Badge bg="light" text="dark">Conducteur de travaux {supervisor.name}</Badge>
                         </div>
-                    )}
-                    {userTypes === 'prestataire' && (
-                        <div className="mt-4">
-                            <h5>Prochaine intervention</h5>
-                            <Badge bg="light" text="dark" className="mt-3">Mercredi 27 octobre 2021 à 13h</Badge>
-                        </div>
-                    )}
-                    <div className="mt-4">
-                        <h5>Responsable</h5>
-                        <p>Jean-Phillipe BTP</p>
-                        <Badge bg="light" text="dark">Conducteur de travaux Jean-Phillipe</Badge>
-                    </div>
-                </Col>
-                <Col>
-                    <h5>{userTypes !== 'prestataire' ? 'Mission en cours' : 'Missions assignées'}</h5>
-                    <Card style={{ boxShadow: `0 0 2px ${theme.primaryLight}` }}>
-                        <Card.Body>
-                            <Card.Title>Électricité</Card.Title>
-                            <Card.Text>Mettre aux normes l'entièreté des bâtiments</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <div style={{
-                top: '2%',
-                right: '0.5%',
-                position: 'absolute',
-            }}><ThreeDotsVertical /></div>
-        </Card.Body>
-    </Card>
-);
+                    </Col>
+                    <Col>
+                        <h4>Mission en cours</h4>
+                        <TaskList id_yard={id_yard} />
+                    </Col>
+                </Row>
+                <div style={{ top: '2%', right: '0.5%', position: 'absolute' }}><ThreeDotsVertical /></div>
+            </Card.Body>
+        </Card>
+    );
+};
 
 CardDashboardChantier.propTypes = {
     userTypes: PropTypes.string,
+    yard: PropTypes.shape(yardShape).isRequired,
 };
 
 export default CardDashboardChantier;
--- src/pages/DashboardChantier/DashboardChantier.jsx
+++ src/pages/DashboardChantier/DashboardChantier.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import { Card, Container, Stack } from 'react-bootstrap';
 import { PlusSquare } from 'react-bootstrap-icons';
-import { Link } from 'react-router-dom';
 
-const DashboardChantier = () => (
-    <Container className="text-center">
-        <Stack gap={4}>
-            <Card>
-                <Card.Body>
-                    <h1>Vos chantiers</h1>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <Link to="/chantiers/nouveau">
-                        <PlusSquare size="4em" />
-                    </Link>
-                </Card.Body>
-            </Card>
-        </Stack>
-    </Container>
-);
+import { CardDashboardChantier } from '../../components';
+
+import { isValidId, request } from '../../utils';
+
+const DashboardChantier = ({ idUser }) => {
+    const [yards, setYards] = React.useState([]);
+
+    React.useEffect(() => {
+        if (isValidId(idUser)) {
+            request.get(`/api/users/${idUser}/yards`).then(r => {
+                setYards(r.data);
+            });
+        }
+    }, [idUser]);
+
+    return (
+        <Container className="text-center">
+            <Stack gap={4} className="mb-5">
+                <Card>
+                    <Card.Body>
+                        <h1>Vos chantiers</h1>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Link to="/chantiers/nouveau">
+                            <PlusSquare size="4em" />
+                        </Link>
+                    </Card.Body>
+                </Card>
+                {yards.map((yard) => (
+                    <CardDashboardChantier key={yard.id_yard} yard={yard} />
+                ))}
+            </Stack>
+        </Container>
+    );
+};
+
+DashboardChantier.propTypes = {
+    /** ID de l'utilisateur connecté */
+    idUser: PropTypes.number.isRequired,
+};
 
 export default DashboardChantier;

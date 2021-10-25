f97f4a021d0826bac2b8670d25551704fd982786
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2021-10-25 23:02:16 +0200

    Link disponibilites (#19)
    * récupération des disponibilités
    
    * sauvegarde des disponibilités et UX

--- src/pages/Availability/Availability.jsx
+++ src/pages/Availability/Availability.jsx
@@ -1,32 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AvailableTimes from 'react-available-times';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Spinner } from 'react-bootstrap';
 
-import { theme } from '../../utils';
+import { Button } from '../../components';
 
-const Availability = () => {
+import { isValidId, request, theme } from '../../utils';
+
+/**
+ * Formate les dates API en objet Date.
+ * @param list {Object[]}
+ * @return {Object[]}
+ */
+const formatDates = (list) => (
+    list.map((item) => ({
+        ...item,
+        start: new Date(item.start),
+        end: new Date(item.end),
+    }))
+);
+
+const Availability = ({ idUser }) => {
+    const [availabilities, setAvailabilities] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isValidId(idUser)) {
+            setLoading(true);
+            request.get(`/api/users/${idUser}/availabilities`).then(r => {
+                setAvailabilities(formatDates(r.data));
+                setLoading(false);
+            });
+        }
+    }, [idUser]);
+
+    const handleSave = React.useCallback(() => {
+        setLoading(true);
+        request.put('/api/availabilities', availabilities).then(r => {
+            setAvailabilities(formatDates(r.data));
+            setLoading(false);
+        });
+    }, [availabilities]);
+
     return (
         <Container>
             <Card className="mb-5" style={{ boxShadow: `0 0 12px ${theme.secondaryDark}` }}>
                 <Card.Body>
-                    <Card.Title as="h2"
-                                style={{ color: theme.primaryDark }}><u><strong>Disponibilités</strong></u></Card.Title>
-                    <Card.Subtitle className="text-muted">
+                    <Card.Title as="h2" style={{ color: theme.primaryDark }}>
+                        <strong>Disponibilités</strong>
+                    </Card.Title>
+                    <Card.Subtitle className="text-muted mb-3">
                         Remplissez les disponibilités de votre entreprise.
                     </Card.Subtitle>
-                    <AvailableTimes
-                        weekStartsOn="monday"
-                        onChange={(selections) => {
-                            selections.forEach(({ start, end }) => {
-                                console.log('Start:', start, 'End:', end);
-                            });
-                        }}
-                    />
+                    <Button variant="success" children="Sauvegarder" onClick={handleSave} loading={loading} />
+                    {!loading ? (
+                        <AvailableTimes
+                            weekStartsOn="monday"
+                            onChange={(selections) => {
+                                console.log(selections);
+                                setAvailabilities(selections);
+                            }}
+                            initialSelections={availabilities}
+                        />
+                    ) : (
+                        <div className="d-flex justify-content-center mt-5 mb-3">
+                            <Spinner animation="border" variant="warning" />
+                        </div>
+                    )}
                 </Card.Body>
             </Card>
         </Container>
     );
+};
+
+Availability.propTypes = {
+    idUser: PropTypes.number.isRequired,
 };
 
 export default Availability;

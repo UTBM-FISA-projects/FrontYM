4011244a7e0d4589c4368c8320a3ae27690e07cf
Valentin <valentin.d77@free.fr>
2021-10-26 12:29:29 +0200

    possibilité de créer un compte

--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -1,13 +1,31 @@
-import React, { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-import { Button, Card, Container, FloatingLabel, Form, Overlay, Stack, Tooltip } from 'react-bootstrap';
+import { Card, Container, FloatingLabel, Form, OverlayTrigger, Stack, Tooltip } from 'react-bootstrap';
 
-import { theme } from '../../utils';
+import { Button } from '../../components';
+
+import sha512 from 'crypto-js/sha512';
+import { request, theme } from '../../utils';
 
 const Inscription = () => {
-    const [show, setShow] = useState(false);
-    const target = useRef(null);
+    const history = useHistory();
+    const [loading, setLoading] = React.useState(false);
+
+    const handleSubmit = React.useCallback((e) => {
+        e.preventDefault();
+        setLoading(true);
+        const data = new FormData(e.target);
+
+        data.set('password', sha512(data.get('password')));
+        data.set('password_confirmation', sha512(data.get('password_confirmation')));
+
+        request.post('/api/users', data).then(() => {
+            // TODO gérer les erreurs
+            history.push('/connexion');
+            setLoading(false);
+        });
+    }, [history]);
 
     return (
         <Container id="bg" className="d-flex justify-content-center align-items-center">
@@ -19,43 +37,48 @@ const Inscription = () => {
                 >
                     Inscription
                 </Card.Header>
-                <Card.Body as={Form}>
-                    <Form.Select required size="lg" className="mb-3">
-                        <option>Qui êtes vous ?</option>
-                        <option value="1">Client</option>
-                        <option value="2">Entreprise</option>
-                    </Form.Select>
+                <Card.Body as={Form} onSubmit={handleSubmit}>
+                    <FloatingLabel label="Qui êtes vous ?">
+                        <Form.Select required name="type" size="lg" className="mb-3">
+                            <option value="project_owner">Client</option>
+                            <option value="enterprise">Entreprise</option>
+                        </Form.Select>
+                    </FloatingLabel>
                     <FloatingLabel label="Nom/Titre" className="mb-3">
-                        <Form.Control required placeholder="Nom/Titre" size="lg" type="text" />
+                        <Form.Control required name="name" placeholder="Nom/Titre" size="lg" type="text" />
                     </FloatingLabel>
                     <FloatingLabel label="Téléphone" className="mb-3">
-                        <Form.Control required placeholder="Téléphone" size="lg" type="phone" />
+                        <Form.Control required name="phone" placeholder="Téléphone" size="lg" type="phone" />
                     </FloatingLabel>
                     <FloatingLabel label="E-mail" className="mb-3">
-                        <Form.Control
-                            required
-                            placeholder="E-mail"
-                            ref={target}
-                            size="lg"
-                            type="email"
-                            onFocus={() => setShow(true)}
-                            onBlur={() => setShow(false)}
-                        />
-                        <Overlay target={target.current} show={show} placement="top">
-                            {(props) => (
-                                <Tooltip {...props}>
-                                    Attention cette adresse e-mail sera votre identifiant !
-                                </Tooltip>
-                            )}
-                        </Overlay>
+                        <OverlayTrigger
+                            placement="top"
+                            trigger={'focus'}
+                            overlay={<Tooltip>Attention cette adresse e-mail sera votre identifiant !</Tooltip>}
+                        >
+                            <Form.Control
+                                required
+                                name="email"
+                                placeholder="E-mail"
+                                size="lg"
+                                type="email"
+                            />
+                        </OverlayTrigger>
                     </FloatingLabel>
                     <Stack direction="horizontal" gap={3}>
                         <FloatingLabel label="Mot de passe" className="mb-3">
-                            <Form.Control required placeholder="Mot de passe" size="lg" type="password" />
+                            <Form.Control
+                                required
+                                name="password"
+                                placeholder="Mot de passe"
+                                size="lg"
+                                type="password"
+                            />
                         </FloatingLabel>
                         <FloatingLabel label="Confirmation du mot de passe" className="mb-3">
                             <Form.Control
                                 required
+                                name="password_confirmation"
                                 placeholder="Confirmation du mot de passe"
                                 size="lg"
                                 type="password"
@@ -63,9 +86,16 @@ const Inscription = () => {
                         </FloatingLabel>
                     </Stack>
                     <Stack gap={2}>
-                        <Button variant="success" type="submit" className="mx-auto px-5">S'inscrire</Button>
+                        <Button
+                            variant="success"
+                            loading={loading}
+                            type="submit"
+                            className="mx-auto px-5"
+                            children="S'inscrire"
+                        />
                         <Button
                             as={Link}
+                            loading={loading}
                             to="/connexion"
                             type="button"
                             variant="outline-secondary"

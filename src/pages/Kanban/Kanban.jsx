0121ea62af248b950237b8740fdb4d0c0209b599
Valentin <valentin.d77@free.fr>
2021-10-26 08:52:50 +0200

    ajout des cartes dans le kanban

--- src/pages/Kanban/Kanban.jsx
+++ src/pages/Kanban/Kanban.jsx
@@ -1,8 +1,12 @@
 import React, { useReducer, useState } from 'react';
+
 import { Button, Card, Col, Container, FloatingLabel, Form, Modal, Row } from 'react-bootstrap';
-import { theme } from '../../utils';
 import ReactQuill from 'react-quill';
 import { DateRangeInput } from '@datepicker-react/styled';
+
+import { theme } from '../../utils';
+import { TaskList } from '../../components/TaskList';
+import PropTypes from 'prop-types';
 
 const colors = {
     secondaryBlue: '#3399ff',
@@ -29,22 +33,9 @@ function reducer(state, action) {
     }
 }
 
-const App = () => {
+const Kanban = ({ id_yard }) => {
+
     const [state, dispatch] = useReducer(reducer, initialState);
-
-    return (
-        <DateRangeInput
-            onDatesChange={data => dispatch({ type: 'dateChange', payload: data })}
-            onFocusChange={focusedInput => dispatch({ type: 'focusChange', payload: focusedInput })}
-            startDate={state.startDate} // Date or null
-            endDate={state.endDate} // Date or null
-            focusedInput={state.focusedInput} // START_DATE, END_DATE or null
-        />
-    );
-};
-
-const Kanban = () => {
-
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -54,97 +45,117 @@ const Kanban = () => {
             <Row>
                 <Col>
                     <Card style={{ borderColor: colors.secondaryBlue }}>
-                        <Card.Header as="h2"
-                                     className="text-center mb-4"
-                                     style={{
-                                         color: colors.secondaryBlue,
-                                         borderColor: colors.secondaryBlue,
-                                         backgroundColor: colors.transparentBlue,
-                                     }}>
+                        <Card.Header
+                            as="h2"
+                            className="text-center mb-4"
+                            style={{
+                                color: colors.secondaryBlue,
+                                borderColor: colors.secondaryBlue,
+                                backgroundColor: colors.transparentBlue,
+                            }}
+                        >
                             À faire
                         </Card.Header>
+                        <TaskList id_yard={id_yard} state="done" />
                         <Card.Body>
                             <div className="d-flex justify-content-end">
                                 <Button variant="success" onClick={handleShow}>Ajouter Mission</Button>
                             </div>
-                            <>
-                                <Modal show={show} onHide={handleClose}>
-                                    <Modal.Body>
-                                        <h2 style={{ color: theme.primaryDark }} className="mb-4">
-                                            <strong>Nouvelle mission</strong>
-                                        </h2>
-                                        <FloatingLabel label="Nom de la mission" className="mb-4">
-                                            <Form.Control required
-                                                          placeholder="Nom de la mission"
-                                                          size="lg"
-                                                          type="text" />
-                                        </FloatingLabel>
-                                        <ReactQuill theme="snow" className="mb-4" />
-                                        <Card.Text className="mb-3">Période estimée</Card.Text>
-                                        {
-                                            App()
-                                        }
-                                        <FloatingLabel label="Temps estimé" className="mt-3 mb-4">
-                                            <Form.Control placeholder="Temps estimé" size="lg" type="text" />
-                                        </FloatingLabel>
-                                        <Card.Text className="mb-3">Entreprise assignée à la mission</Card.Text>
-                                        <Form.Select aria-label="Entreprise assignée" className="mb-3">
-                                            <option>Entreprise assignée</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                            <option value="4">Viva l'algérie !</option>
-                                        </Form.Select>
-                                        <div className="d-flex justify-content-end">
-                                            <Button variant="danger"
-                                                    onClick={handleClose}
-                                                    style={{ marginRight: '1em' }}>
-                                                Annuler
-                                            </Button>
-                                            <Button variant="success" onClick={handleClose}>
-                                                Ajouter
-                                            </Button>
-                                        </div>
-                                    </Modal.Body>
-                                </Modal>
-                            </>
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Body>
+                                    <h2 style={{ color: theme.primaryDark }} className="mb-4">
+                                        <strong>Nouvelle mission</strong>
+                                    </h2>
+                                    <FloatingLabel label="Nom de la mission" className="mb-4">
+                                        <Form.Control
+                                            required
+                                            placeholder="Nom de la mission"
+                                            size="lg"
+                                            type="text"
+                                        />
+                                    </FloatingLabel>
+                                    <ReactQuill theme="snow" className="mb-4" />
+                                    <Card.Text className="mb-3">Période estimée</Card.Text>
+                                    <DateRangeInput
+                                        onDatesChange={data => dispatch({ type: 'dateChange', payload: data })}
+                                        onFocusChange={focusedInput => dispatch({
+                                            type: 'focusChange',
+                                            payload: focusedInput,
+                                        })}
+                                        startDate={state.startDate} // Date or null
+                                        endDate={state.endDate} // Date or null
+                                        focusedInput={state.focusedInput} // START_DATE, END_DATE or null
+                                    />
+                                    <FloatingLabel label="Temps estimé" className="mt-3 mb-4">
+                                        <Form.Control placeholder="Temps estimé" size="lg" type="text" />
+                                    </FloatingLabel>
+                                    <Card.Text className="mb-3">Entreprise assignée à la mission</Card.Text>
+                                    <Form.Select aria-label="Entreprise assignée" className="mb-3">
+                                        <option>Entreprise assignée</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                        <option value="4">Viva l'algérie !</option>
+                                    </Form.Select>
+                                    <div className="d-flex justify-content-end">
+                                        <Button variant="danger"
+                                                onClick={handleClose}
+                                                className="me-3"
+                                        >
+                                            Annuler
+                                        </Button>
+                                        <Button variant="success" onClick={handleClose}>
+                                            Ajouter
+                                        </Button>
+                                    </div>
+                                </Modal.Body>
+                            </Modal>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card style={{ borderColor: theme.primaryDark }}>
-                        <Card.Header as="h2"
-                                     className="text-center mb-4"
-                                     style={{
-                                         color: theme.primaryDark,
-                                         backgroundColor: colors.transparentYellow,
-                                         borderColor: theme.primaryDark,
-                                     }}>
+                        <Card.Header
+                            as="h2"
+                            className="text-center mb-4"
+                            style={{
+                                color: theme.primaryDark,
+                                backgroundColor: colors.transparentYellow,
+                                borderColor: theme.primaryDark,
+                            }}
+                        >
                             En cours
                         </Card.Header>
                         <Card.Body>
+                            <TaskList id_yard={id_yard} state="doing" />
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card style={{ borderColor: colors.secondaryGreen }}>
-                        <Card.Header as="h2"
-                                     className="text-center mb-4"
-                                     style={{
-                                         color: colors.secondaryGreen,
-                                         borderColor: colors.secondaryGreen,
-                                         backgroundColor: colors.transparentGreen,
-                                     }}>
+                        <Card.Header
+                            as="h2"
+                            className="text-center mb-4"
+                            style={{
+                                color: colors.secondaryGreen,
+                                borderColor: colors.secondaryGreen,
+                                backgroundColor: colors.transparentGreen,
+                            }}
+                        >
                             Fait
                         </Card.Header>
                         <Card.Body>
-
+                            <TaskList id_yard={id_yard} state="done" />
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
         </Container>
     );
+};
+
+Kanban.propTypes = {
+    id_yard: PropTypes.number.isRequired,
 };
 
 export default Kanban;

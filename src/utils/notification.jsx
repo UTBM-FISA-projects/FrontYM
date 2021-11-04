ff7097ffc40b98c6b0d87bfafd3c4a80f449018b
Valentin <valentin.d77@free.fr>
2021-11-04 11:47:20 +0100

    gestion des notifications lu

--- src/utils/notification.jsx
+++ src/utils/notification.jsx
@@ -4,26 +4,60 @@ import React from 'react';
 import { Stack } from 'react-bootstrap';
 
 import { Button } from '../components';
+import request from './request';
+import isValidId from './isValidId';
+
+/**
+ * Envoie une requête pour marquer une notification comme lu.
+ * @param id {number}
+ */
+const setRead = (id) => {
+    if (isValidId(id)) {
+        request.put(`/api/notifications/${id}/read`, null);
+    }
+};
+
+/**
+ * Notification supprimée.
+ *
+ * @param id {number}
+ * @return {JSX.Element}
+ */
+const DeletedNotification = (id) => (
+    <span
+        onClick={() => {setRead(id);}}
+        className="text-muted"
+    >
+        Notification supprimée
+    </span>
+);
 
 const notification = {
 
     /**
+     * Affiche la notification d'un proposition de chantier.
      *
      * @param notif {Object}
      * @return {JSX.Element|string}
      */
     proposition: (notif) => {
-        let template = notif.notification_type.template;
+        const {
+            id_notification,
+            notification_type,
+            parameters,
+        } = notif;
+
+        let template = notification_type.template;
 
         try {
-            template = template.replace('${project_owner}', notif.parameters.project_owner.name);
-            template = template.replace('${yard}', notif.parameters.yard.name);
+            template = template.replace('${project_owner}', parameters.project_owner.name);
+            template = template.replace('${yard}', parameters.yard.name);
         } catch (e) {
-            return <span className="text-muted">Notification supprimée</span>
+            return DeletedNotification(id_notification);
         }
 
         return (
-            <div className="my-2" onClick={() => {console.log("read");}}>
+            <div className="my-2" onClick={() => {setRead(id_notification);}}>
                 <div className="mb-2">{template}</div>
                 <Stack direction="horizontal" gap={2} className="d-flex justify-content-center">
                     <Button
@@ -44,36 +78,50 @@ const notification = {
     },
 
     /**
+     * Notification d'une proposition de mission.
      *
      * @param notif {Object}
      * @return {JSX.Element|string}
      */
     proposition_mission: (notif) => {
-        let template = notif.notification_type.template;
+        const {
+            id_notification,
+            notification_type,
+            parameters,
+        } = notif;
+
+        let template = notification_type.template;
 
         try {
-            template = template.replace('${enterprise}', notif.parameters.enterprise.name);
-            template = template.replace('${task}', notif.parameters.task.title);
-            template = template.replace('${yard}', notif.parameters.yard.name);
+            template = template.replace('${enterprise}', parameters.enterprise.name);
+            template = template.replace('${task}', parameters.task.title);
+            template = template.replace('${yard}', parameters.yard.name);
         } catch (e) {
-            return <span className="text-muted">Notification supprimée</span>;
+            return DeletedNotification(id_notification);
         }
 
         return template;
     },
 
     /**
+     * Notification du dépassement du temps estimé d'une tache.
      *
      * @param notif {Object}
      * @return {JSX.Element|string}
      */
     overtime: (notif) => {
-        let template = notif.notification_type.template;
+        const {
+            id_notification,
+            notification_type,
+            parameters,
+        } = notif;
+
+        let template = notification_type.template;
 
         try {
-            template = template.replace('${task}', notif.parameters.task.title);
+            template = template.replace('${task}', parameters.task.title);
         } catch (e) {
-            return <span className="text-muted">Notification supprimée</span>;
+            return DeletedNotification(id_notification);
         }
 
         return template;

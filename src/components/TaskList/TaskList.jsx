5607dfd1fbd29e67ff5f90745f11f97d532540b0
Valentin <valentin.d77@free.fr>
2021-11-24 11:03:43 +0100

    pagination kanban

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,38 +5,65 @@ import { Badge, Card } from 'react-bootstrap';
 
 import { date, isValidId, request, theme } from '../../utils';
 import { EstimatedTime } from '../EstimatedTime';
+import { Button } from '../Button';
 
 const TaskList = ({ id_yard, state }) => {
     const [tasks, setTasks] = React.useState([]);
 
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const [lastPage, setLastPage] = React.useState(1);
+    const [loading, setLoading] = React.useState(false);
+
     React.useEffect(() => {
         if (isValidId(id_yard)) {
-            request.get(`/api/yards/${id_yard}/tasks?state=${state}`).then(r => {
-                setTasks(r.data);
+            setLoading(true);
+
+            request.get(`/api/yards/${id_yard}/tasks`, { state, page: currentPage }).then(r => {
+                setTasks((prev) => ([...prev, ...r.data]));
+                setCurrentPage(r.current_page);
+                setLastPage(r.last_page);
+                setLoading(false);
             });
         }
-    }, [id_yard, state]);
+    }, [currentPage, id_yard, state]);
 
-    return tasks.map((task) => (
-        <Card key={task.id_task}
-              className="my-3 mx-2 overflow-hidden"
-              style={{ boxShadow: `0 0 2px ${theme.primaryLight}` }}>
-            <Card.Body>
-                <Card.Title as="h6" className="d-flex justify-content-between">
-                    {task.title}
-                    <small className="fs-6">
-                        <EstimatedTime spentTime={task.time_spent} estimatedTime={task.estimated_time} />
-                    </small>
-                </Card.Title>
-                <Badge className="mb-2 text-wrap" bg="light" text="secondary">
-                    Début prévu : {date.long(task.start_planned_date)}
-                </Badge>
-                <Card.Text className="lh-1 text-muted">
-                    <small dangerouslySetInnerHTML={{ __html: task.description }} />
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    ));
+    return (
+        <>
+            {tasks.map((task) => (
+                <Card key={task.id_task}
+                      className="my-3 mx-2 overflow-hidden"
+                      style={{ boxShadow: `0 0 2px ${theme.primaryLight}` }}>
+                    <Card.Body>
+                        <Card.Title as="h6" className="d-flex justify-content-between">
+                            {task.title}
+                            <small className="fs-6">
+                                <EstimatedTime spentTime={task.time_spent} estimatedTime={task.estimated_time} />
+                            </small>
+                        </Card.Title>
+                        {task.start_planned_date && (
+                            <Badge className="mb-2 text-wrap" bg="light" text="secondary">
+                                Début prévu : {date.long(task.start_planned_date)}
+                            </Badge>
+                        )}
+                        <Card.Text className="lh-1 text-muted">
+                            <small dangerouslySetInnerHTML={{ __html: task.description }} />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            ))}
+            {currentPage !== lastPage && (
+                <div className="d-flex justify-content-center">
+                    <Button
+                        loading={loading}
+                        children="Charger plus"
+                        variant="outline-secondary"
+                        size="sm"
+                        onClick={() => {setCurrentPage(currentPage + 1);}}
+                    />
+                </div>
+            )}
+        </>
+    );
 };
 
 TaskList.propTypes = {
